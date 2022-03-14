3cd46c1a34d07e43015eb92f3162540e8dd75973
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2022-03-14 16:33:25 +0800

    debug

--- actions.go
+++ actions.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 )
 
 func addXP(guildID string, userID string, nickname string, avatar string) (err error) {
 	toAdd := rand.Int63n(11) + 15
+	fmt.Println(nickname)
+	fmt.Println(avatar)
 	_, err = pool.Exec(context.Background(), "INSERT INTO guildXP (guildID, userID, nickname, avatar, xp) VALUES ($1, $2, $3, $4, $5) ON CONFLICT (guildID, userID) DO UPDATE SET xp = guildXP.xp + EXCLUDED.xp, nickname = EXCLUDED.nickname, avatar = EXCLUDED.avatar", guildID, userID, nickname, avatar, toAdd)
 	return
 }
